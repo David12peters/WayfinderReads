188e96bda0d403d2509c90f2fc6fb2404a7f1681
David Peters <104854750+David12peters@users.noreply.github.com>
2024-10-13 15:56:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect, useContext, useRef } from 'react';
+import './App.css';
+import './assets/style.css';
+import ProductImgPray from './assets/Pray.jpg';
+import ProductImgLogo from './assets/Logo.png';
 import CancelPage from './components/CancelPage';
 import SuccessPage from './components/SuccessPage';
+import { useFlutterwave, closePaymentModal } from 'flutterwave-react-v3';
+import { useNavigate } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';  
+
 
 
 const App = () => {
-    const [fullscreenIframe, setFullscreenIframe] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [displayedProducts, setDisplayedProducts] = useState([]);
+    const [fullscreenIframe, setFullscreenIframe] = useState(null);
     const [isMinimized, setIsMinimized] = useState(true);
     const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
-
-
+    const iframeRef = useRef(null); // To hold the reference to the iframe element
+    const infoPanelRef = useRef(null);
+    const headerRef = useRef(null);
+    const navigate = useNavigate();
+    const [isProcessing, setIsProcessing] = useState(false);
+   
     // Dummy data for products (assuming this is fetched or passed in reality)
     const products = [
-        { title: 'Product 1', price: '$10', image: 'image1.jpg' },
-        { title: 'Product 2', price: '$20', image: 'image2.jpg' },
-        // Add more products here
-    ];
+        { id: 1, title: 'Christ Jesus our royal highpriest', price: '$2', url: 'https://drive.google.com/file/d/1Sqe5wME6DAzXkv9Z21KQIP9QDlpEzIQz/preview?usp=embed_googleplus' },
+        { id: 2, title: '21 Irrefutable Laws of LEADERSHIP', price: '$2', url: 'https://drive.google.com/file/d/1T-F_th0WpJuq0b_KeQwo7hu1B5vKkKv6/preview?usp=embed_googleplus' },
+        { id: 3, title: 'Adult Children of Emotionally Immature Parents', price: '$2', url: 'https://drive.google.com/file/d/1T8ok_wVWG5v8UViey50OTcFbqAhX93b9/preview?usp=embed_googleplus' },
+        { id: 4, title: 'TODAY MATTERS_ 12 Daily Practices To Guarantee Tomorrow\'s Success', price: '$2', url: 'https://drive.google.com/file/d/1TAc3-e6QYYHFelcOE4cnsIBKYv9FwN0t/preview?usp=embed_googleplus' },
+        { id: 5, title: 'Battlefield of the Mind - Joyce Meyer', price: '$2', url: 'https://drive.google.com/file/d/1TDdPhMjOsJihAWWL2Kt207dtBytK5qRQ/preview?usp=embed_googleplus' },
+        { id: 6, title: 'Becoming a Praise Warrior', price: '$2', url: 'https://drive.google.com/file/d/1TDoB0CSM7M94oy-GgKAV6ZCberUY6Asu/preview?usp=embed_googleplus' },
+        { id: 7, title: 'BEYOND THE NAKED', price: '$2', url: 'https://drive.google.com/file/d/1TJId7JccxmjjdzSytkkX_LXlBo25bOVa/preview?usp=embed_googleplus' },
+        { id: 8, title: 'Binding the Strong Man - Nicholas Duncan-Williams', price: '$2', url: 'https://drive.google.com/file/d/1TKNqc2sZ60xLtR0RKfLxFC_rCi_6rO9M/preview?usp=embed_googleplus' },
+        { id: 9, title: 'Church Mafia - Makhado Sinthumule', price: '$2', url: 'https://drive.google.com/file/d/1TX96WapLBlyqT0bqdVqCBDCaQpSM_-Mc/preview?usp=embed_googleplus' },
+        { id: 10, title: 'Demons: The Answer Book - Lester Sumrall', price: '$2', url: 'https://drive.google.com/file/d/1Tg0FDiusVaMHmN5IctdH73Tvh6hOqM2C/preview?usp=embed_googleplus' },
+        { id: 11, title: 'Divine Timing - Nicholas Duncan-Williams', price: '$2', url: 'https://drive.google.com/file/d/1TkEFLCCC4QuwUOHnqnjGmyXM0kVZ962u/preview?usp=embed_googleplus' },
+        { id: 12, title: 'Enforcing Prophetic Decrees - Nicholas Duncan-Williams', price: '$2', url: 'https://drive.google.com/file/d/1UATux7Se4H5ybXnyNhduX2NUoHmRwTtZ/preview?usp=embed_googleplus' },
+        { id: 13, title: 'Erica Part 1', price: '$2', url: 'https://drive.google.com/file/d/1WlQg4Z3EkU2Mj8zqxSb5lR7ZPD94fUDV/preview?usp=embed_googleplus' },
+        { id: 14, title: 'Erica Part 2', price: '$2', url: 'https://drive.google.com/file/d/1V7X3kVn6bDWk_P4d6Rj2xcTRg_aJOSMa/preview?usp=embed_googleplus' },
+        { id: 15, title: 'Erica Part Three Witchcraft Spiritual Warfare (Book 3 of the Erica Testimonial Series)', price: '$2', url: 'https://drive.google.com/file/d/1V965wcXPekAS4Ib6vhEfCk3Umb3onVCD/preview?usp=embed_googleplus' },
+        { id: 16, title: 'Find Your Why: A Practical Guide for Discovering Purpose for You and Your Team by Simon Sinek', price: '$2', url: 'https://drive.google.com/file/d/1V9e5f1o9ZFnQC1SxjShwRIE2izVdyu3k/preview?usp=embed_googleplus' },
+        { id: 17, title: 'God\'s Generals: The Revivalists', price: '$2', url: 'https://drive.google.com/file/d/1VFAw2gPRzye1EBDPxdayv7857gG7AnL4/preview?usp=embed_googleplus' },
+        { id: 18, title: 'God\'s Generals: Charles Finney', price: '$2', url: 'https://drive.google.com/file/d/1VGvTbuXqposa2hwBYjIe4xqTimLHiqhf/preview?usp=embed_googleplus' },
+        { id: 19, title: 'God\'s General', price: '$2', url: 'https://drive.google.com/file/d/1VL25SyVWftilg7EnWT-LxpKqcPbe7LaB/preview?usp=embed_googleplus' },
+        { id: 20, title: 'Healing Belongs to Us by Kenneth E. Hagin', price: '$2', url: 'https://drive.google.com/file/d/1VNjrRRP3mEwP5uNZEEeZJa7gkDw6M2i8/preview?usp=embed_googleplus' },
+        { id: 21, title: 'I Went to Hell - Kenneth Hagin', price: '$2', url: 'https://drive.google.com/file/d/1VUy2nqHsb9AcNn4tu-P09nfHpykJpPdn/preview?usp=embed_googleplus' },
+        { id: 22, title: 'Reaching the Lost: Evangelism by Bobby Jamieson', price: '$2', url: 'https://drive.google.com/file/d/1VdeGyq6trG9MXm7Z5DHpzzmD8h_brjBs/preview?usp=embed_googleplus' },
+        { id: 23, title: 'The Emojis', price: '$2', url: 'https://drive.google.com/file/d/1mZ2mxVk3CXirbmDinOivE0Dl_0Oqv2c5/preview?usp=embed_googleplus' },
+        { id: 24, title: 'The Miracle Seed - David O. Oyedepo', price: '$2', url: 'https://drive.google.com/file/d/1VqxfulKcacCH8b3zo59BPp9JzFRJwnxr/preview?usp=embed_googleplus' },
+        { id: 25, title: 'The Visions', price: '$2', url: 'https://drive.google.com/file/d/1mZsoV1v90jWnKLpTHUdJIYl4zM89UE8t/preview' },
+        { id: 26, title: 'The Price of Greatness - Nicholas Duncan-Williams', price: '$2', url: 'https://drive.google.com/file/d/1VrLF9Hd_Gz2Tt0rTTTk2Rx7mhB9vc652/preview?usp=embed_googleplus' },
+        { id: 27, title: 'Understanding the Power of Praise - David Oyedepo', price: '$2', url: 'https://drive.google.com/file/d/1VsxkhQIYxudXg7V8T22OQaiASaf51PrB/preview?usp=embed_googleplus' },
+        { id: 28, title: 'When God Writes Your Love Story (Expanded Edition)', price: '$2', url: 'https://drive.google.com/file/d/1Vzgd7I5j8D0es32t4Qlm5LffDx-0cHfG/preview?usp=embed_googleplus' },
+        { id: 29, title: 'Who is this Allah? - GJO Moshay', price: '$2', url: 'https://drive.google.com/file/d/1W0iafNZs0oN9_yMStU94-AzFWqCM2zxr/preview?usp=embed_googleplus' },
+        { id: 30, title: 'Why You Act the Way You Do - Tim Lahaye', price: '$2', url: 'https://drive.google.com/file/d/1W8nmBicW3uUq8BPEpeq2u0p_qvP-mvXO/preview?usp=embed_googleplus' }
+         
+        ];
 
 
-    // Video sources
-    const videoSources = [
-        'https://www.youtube.com/embed/dQw4w9WgXcQ', // Video 1
-        'https://www.youtube.com/embed/9bZkp7q19f0',  // Video 2
-        // Add more video sources here
-    ];
+    // Function to load products initially
+    useEffect(() => {
+        setDisplayedProducts(products);
+    }, []);
+
+
+    // Function to filter products based on search query
+    const handleSearch = (query) => {
+        const filteredProducts = products.filter(product =>
+            product.title.toLowerCase().includes(query.toLowerCase())
+        );
+        setDisplayedProducts(filteredProducts);
+    };
+
+
+
+            
+    
+    // Array of iframe video sources
+const videoSources = [
+    'https://www.youtube.com/embed/JxOuQxq5AOg?si=MBBCrqRAVjza4P7i',
+    'https://www.youtube.com/embed/bwD99EqbTKQ?si=-f6L6QX3Xrgz_-Hv',
+    'https://www.youtube.com/embed/rnHldmO4vdk?si=5wEyWNlcwt_4lzN1',
+    'https://www.youtube.com/embed/J6OTCWSurYQ?si=3D5zWsloCKFnwc-J',
+    'https://www.youtube.com/embed/ujqiec1bAds?si=oqw-5FZ-8p-hfIWb',
+    'https://www.youtube.com/embed/25LO-SCcD4c?si=LHy-zjQExf4QswaN',
+    'https://www.youtube.com/embed/25LO-SCcD4c?si=sPx7A2oMikPdLp_5',
+    'https://www.youtube.com/embed/l7C4_v4Lnxc?si=lqt2aVK__4wBUupj',
+  ];
+
 
 
     // Function to switch video every 30 minutes
@@ -49,11 +116,193 @@ const App = () => {
     };
 
 
-    const addProductToCart = (product) => {
-        // Handle adding product to the cart
-        console.log(`Added ${product.title} to the cart.`);
+    
+    const option = () => {
+        const infoPanel = infoPanelRef.current;
+        if (infoPanel) {
+            infoPanel.style.display = 'block';
+        }
+        headerRef.style.height = '600px'
     };
 
+
+    const hideInfoPanel = () => {
+        const infoPanel = infoPanelRef.current;
+        if (infoPanel) {
+            infoPanel.style.display = 'none';
+        }
+        headerRef.style.height = '150px'
+    };
+
+
+    
+
+
+    const handleShare = () => {
+        if (navigator.share) {
+            navigator.share({
+                title: 'Check out this book!',
+                url: window.location.href
+            }).catch(console.error);
+        } else {
+            alert("Sharing is not supported in your browser.");
+        }
+    };
+
+
+    const handleContact = () => {
+        // Scroll to contact section or open a modal
+        const contactSection = document.getElementById('contact');
+        if (contactSection) {
+            contactSection.scrollIntoView({ behavior: 'smooth' });
+        } else {
+            window.location.href = "mailto:davidoluwaseun874@gmail.com"; // Update with the actual email
+        }
+    };
+
+
+    // Cart logic: Load, save, and handle updates
+    const saveCartItems = (items) => {
+        localStorage.setItem('cartItems', JSON.stringify(items));
+    };
+
+
+   const loadCartItems = () => {
+        const storedItems = localStorage.getItem('cartItems');
+        const storedTotal = localStorage.getItem('cartTotal');
+
+
+        if (storedItems) {
+            setCartItems(JSON.parse(storedItems));
+        }
+        if (storedTotal) {
+            setTotal(parseFloat(storedTotal));
+        }
+    };
+
+
+    const calculateTotal = (items) => {
+        const newTotal = items.reduce((acc, item) => {
+            return acc + parseFloat(item.price.replace('$', '')) * item.quantity;
+        }, 0);
+        setTotal(newTotal);
+        localStorage.setItem('cartTotal', newTotal.toFixed(2));
+    };
+
+
+    const addProductToCart = (product) => {
+        const existingItem = cartItems.find(item => item.id === product.id);
+        if (existingItem) {
+            alert('This item is already in the cart');
+        } else {
+            const newItems = [...cartItems, { ...product, quantity: 1 }];
+            setCartItems(newItems);
+            saveCartItems(newItems);
+            calculateTotal(newItems);
+        }
+    };
+
+
+    const removeProductFromCart = (productId) => {
+        const newItems = cartItems.filter(item => item.id !== productId);
+        setCartItems(newItems);
+        saveCartItems(newItems);
+        calculateTotal(newItems);
+    };
+
+
+    const updateProductQuantity = (productId, quantity) => {
+        if (quantity < 1) return;
+        const newItems = cartItems.map(item =>
+            item.id === productId ? { ...item, quantity } : item
+        );
+        setCartItems(newItems);
+        saveCartItems(newItems);
+        calculateTotal(newItems);
+    };
+
+
+    // Toggles the cart visibility
+    const toggleCart = () => {
+        setIsActive(!isActive);
+    };
+
+
+    const getTotalQuantity = () => {
+        return cartItems.reduce((acc, item) => acc + item.quantity, 0);
+    };
+
+
+    useEffect(() => {
+        loadCartItems();
+    }, []);
+    
+    useEffect(() => {
+        // Update the cart icon's data-quantity attribute
+        const cartIcon = document.getElementById('cart-icon');
+        if (cartIcon) {
+            cartIcon.setAttribute('data-quantity', getTotalQuantity());
+        }
+    }, [cartItems]);
+
+
+
+
+
+
+    const config = {
+        public_key: process.env.REACT_APP_FLUTTERWAVE_PUBLIC_KEY,
+        tx_ref: Date.now(),
+        amount: total.toFixed(2) * 1700,
+        currency: 'NGN',
+        payment_options: 'card,mobilemoney,ussd',
+        customer: {
+          email: 'davidoluwaseun874@gmail.com',
+           phone_number: '08087846847',
+          name: 'David Peters',
+        },
+        customizations: {
+          title: 'Books Purchase',
+          description: 'Payment for items bought in cart',
+          logo: {ProductImgLogo},
+        },
+      };
+    
+      const handleFlutterPayment = useFlutterwave(config);
+
+
+  const handlePaymentSuccess = (response) => {
+    console.log("Payment Successful:", response);
+    // Verify payment on the server to ensure it's valid
+    closePaymentModal(); // Closes the payment modal programmatically
+    navigate("/success"); // Redirect to SuccessPage after payment
+  };
+
+
+  const handlePaymentFailure = (response) => {
+    console.log("Payment Failed or Cancelled:", response);
+    closePaymentModal(); // Closes the payment modal programmatically
+    navigate("/cancel"); // Redirect to CancelPage after failure
+  };
+
+
+  const handlePayment = () => {
+    setIsProcessing(true); // Show loading state during payment process
+    handleFlutterPayment({
+      callback: (response) => {
+        if (response.status === "successful") {
+          handlePaymentSuccess(response);
+        } else {
+          handlePaymentFailure(response);
+        }
+      },
+      onClose: () => {
+        // Handle case where user closes the modal without completing payment
+        console.log("Payment modal closed without completing the transaction.");
+        handlePaymentFailure();
+      },
+    });
+  };
 
     return (
         <div className="app">
